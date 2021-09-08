2c7f132a83ef0d83beca4a4034589c741d350474
Ryan_LEE <rab7890@gmail.com>
2021-09-08 22:30:43 +0900

    1. 행사 관리 페이지 및 기능 추가

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('ajax/member/search', views.ajax_search_member, name='ajax_search_member'),
 
     path('event', views.event_mgr_view, name='event'),
+    path('event/register', views.event_register_view, name='register_event'),
+    path('ajax/event/member/search', views.ajax_search_member_by_event, name='ajax_search_group_event'),
+    path('ajax/event/modify', views.ajax_modify_group_event, name='ajax_modify_event'),
+    path('ajax/event/add', views.ajax_add_group_event, name='ajax_add_event'),
 
     path('login', views.log_in, name='login'),
     path('logout', views.log_out, name='logout'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -8,10 +8,13 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import authenticate, logout
+from rest_framework import serializers
 
 from mainapp.forms import MemberForm
-from mainapp.models import GroupType, Member, Staff
+from mainapp.models import GroupType, Member, Staff, GroupEvent
 from django.contrib.auth import login
+from django.db.models import CharField, Value
+
 
 @login_required(login_url='mainapp:login')
 def member_mgr_view(request):
@@ -21,23 +24,22 @@ def member_mgr_view(request):
     context['staff'] = s
     return render(request, 'mainapp/member_mgr.html', context=context)
 
-@login_required(login_url='mainapp:login')
-def event_mgr_view(request):
-    context = {}
-    u = request.user
-    s = Staff.objects.get(user=u)
-    context['staff'] = s
-    context['participate'] = ['위문품(설)', '위문품(보훈)', '위문품(추석)', '위문품(기타1)', '위문품(기타2)', '위문품(기타2)',
-                              '전적지순례', '체련대회']
 
-    return render(request, 'mainapp/event_mgr.html', context=context)
 
 @login_required(login_url='mainapp:login')
 def member_detail_view(request, pk):
     context = {}
-    obj = Member.objects.get(id=pk)
-    context['member'] = obj
-    return render(request, 'mainapp/member_detail.html', context=context)
+    if request.method == "GET":
+        obj = Member.objects.get(id=pk)
+        context['form'] = MemberForm(instance=obj)
+        context['member'] = obj
+        return render(request, 'mainapp/member_detail.html', context=context)
+    else:
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            return HttpResponse('<script type="text/javascript">window.close(); location.reload();</script>')
 
 @csrf_protect
 @login_required(login_url='mainapp:login')
@@ -106,3 +108,103 @@ def ajax_search_member(request):
 
         data = serialize("json", member)
         return JsonResponse({'status': 'success', 'data': data})
+
+class GroupEventSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = GroupEvent
+        fields = '__all__'
+        depth = 2
+
+
+@login_required(login_url='mainapp:login')
+def event_mgr_view(request):
+    context = {}
+    u = request.user
+    s = Staff.objects.get(user=u)
+    context['staff'] = s
+    context['participate'] = ['위문품(설)', '위문품(보훈)', '위문품(추석)', '위문품(기타1)', '위문품(기타2)', '위문품(기타3)',
+                              '전적지순례', '체련대회']
+
+    return render(request, 'mainapp/event_mgr.html', context=context)
+
+@login_required(login_url='mainapp:login')
+def event_register_view(request):
+    context = {}
+    u = request.user
+    s = Staff.objects.get(user=u)
+    context['staff'] = s
+
+    return render(request, 'mainapp/event_register.html', context=context)
+
+
+def ajax_search_member_by_event(request):
+    if request.method == "GET":
+        ck_group = request.GET.getlist('ck_group[]')
+        ck_participant = request.GET.getlist('ck_participant[]')
+        event_date = request.GET.get('event_date')
+        member_name = request.GET.get('member_name')
+        fileds = {'위문품(설)': 'sul', '위문품(보훈)': 'bohoon', '위문품(추석)': 'choosuk', '위문품(기타1)': 'etc1',
+                  '위문품(기타2)': 'etc2', '위문품(기타3)':'etc3', '전적지순례': 'junjukji', '체련대회': 'charun'}
+        d = {}
+
+        for k in fileds:
+            if k in ck_participant:
+                d[fileds[k]] = True
+            else:
+                d[fileds[k]] = False
+
+        ge = GroupEvent.objects.filter(member__group_type__name__in=ck_group, member__name=member_name,
+                                       event_created_date=event_date, **d)
+
+        data = GroupEventSerializer(ge, many=True)
+
+        return JsonResponse({'status': 'success', 'data': data.data})
+
+@csrf_protect
+def ajax_modify_group_event(request):
+    if request.method == "POST":
+
+        gid = request.POST.get('gid')
+        sul = request.POST.get('ck_sul')
+        bohoon = request.POST.get('ck_bohoon')
+        choosuk = request.POST.get('ck_choosuk')
+        etc1 = request.POST.get('ck_etc1')
+        etc2 = request.POST.get('ck_etc2')
+        etc3 = request.POST.get('ck_etc3')
+        junjukji = request.POST.get('ck_junjukji')
+        charun = request.POST.get('ck_charun')
+        ge = GroupEvent.objects.get(id=gid)
+        ge.sul = sul
+        ge.bohoon = bohoon
+        ge.choosuk = choosuk
+        ge.etc1 = etc1
+        ge.etc2 = etc2
+        ge.etc3 = etc3
+        ge.junjukji = junjukji
+        ge.charun = charun
+        ge.save()
+
+        return JsonResponse({'status': 'success', 'error': ""})
+
+@csrf_protect
+def ajax_add_group_event(request):
+    if request.method == "POST":
+
+        id_list = request.POST.getlist('id_list[]')
+        event_date = request.POST.get('event_date')
+        for pk in id_list:
+            ge = GroupEvent()
+            ge.member_id = pk
+            ge.event_created_date = event_date
+            ge.sul = False
+            ge.bohoon = False
+            ge.choosuk = False
+            ge.etc1 = False
+            ge.etc2 = False
+            ge.etc3 = False
+            ge.junjukji = False
+            ge.charun = False
+            ge.save()
+
+        return JsonResponse({'status': 'success', 'error': ""})
